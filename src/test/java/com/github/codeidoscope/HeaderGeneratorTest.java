f31845b4d41e09707aea71010a0917c434aa9546
Marion LV <marion@8thlight.com>
2019-01-30 13:45:10 +0000

    Remove irrelevant field

--- src/test/java/com/github/codeidoscope/HeaderGeneratorTest.java
+++ src/test/java/com/github/codeidoscope/HeaderGeneratorTest.java
@@ -9,7 +9,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class HeaderGeneratorTest {
-    private StatusCodes statusCodes;
 
     @Test
     void aNewHeaderObjectWithCorrectDataIsCreated() {
